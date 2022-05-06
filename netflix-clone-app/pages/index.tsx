ed868fc6682d97b91e37b072414aa2a63a3a8c00
BrandonHanekom <brandonhanekom00@gmail.com>
2022-05-06 19:38:53 +0200

    banner blend and scrolling

--- netflix-clone-app/pages/index.tsx
+++ netflix-clone-app/pages/index.tsx
@@ -37,14 +37,14 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-24">
         <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           {/* My List */}
-          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Action" movies={actionMovies} />
           <Row title="Comedies" movies={comedyMovies} />
-          <Row title="Scary Movies" movies={horrorMovies} />
-          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Horror" movies={horrorMovies} />
+          <Row title="Romance" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
