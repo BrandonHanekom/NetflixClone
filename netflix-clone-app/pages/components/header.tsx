0862fa8bd659220826d3bc7c2c6101797259f760
BrandonHanekom <brandonhanekom00@gmail.com>
2022-05-06 22:26:46 +0200

    tried updating other components, everything still broken

--- netflix-clone-app/pages/components/header.tsx
+++ netflix-clone-app/pages/components/header.tsx
@@ -1,5 +1,7 @@
-import { SearchIcon, BellIcon } from '@heroicons/react/solid'
+import Image from 'next/image'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -14,10 +16,12 @@ function Header() {
     }
 
     window.addEventListener('scroll', handleScroll)
+
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
   return (
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
@@ -27,16 +31,19 @@ function Header() {
           height={100}
           className="cursor-pointer object-contain"
         />
+
         <ul className="hidden space-x-4 md:flex">
-          <li className="headerList">Home</li>
-          <li className="headerList">Movies</li>
-          <li className="headerList">TV Shows</li>
-          <li className="headerList">What's Hot</li>
-          <li className="headerList">My List</li>
+          <li className="headerLink cursor-default font-semibold text-white hover:text-white">
+            Home
+          </li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
         </ul>
       </div>
       <div className="flex items-center space-x-4 text-sm font-light">
-        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
       </div>
